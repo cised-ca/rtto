df8e67c7fe4cbb379e05084c4ca720eb477c82c1
Chimo <chimo@chromic.org>
2017-10-30 15:59:14 -0400

    Implement logo

--- src/components/SiteFooterComponent.js
+++ src/components/SiteFooterComponent.js
@@ -8,24 +8,26 @@ class SiteFooterComponent extends React.Component {
   render() {
     return (
       <footer className="site__footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <h2>Contact</h2>
+        <div className="site__footer-in">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <h2>Contact</h2>
 
-              <p>
-                The Centre for Social Enterprise Development<br />
-                7 Bayview Road<br />
-                Ottawa, ON  K1Y 2C5<br />
-                <a href="mailto:team@csedottawa.ca">team@csedottawa.ca</a><br />
-                <a href="https://twitter.com/csedottawa">@csedottawa</a>
-              </p>
-            </div>
+                <p>
+                  The Centre for Social Enterprise Development<br />
+                  7 Bayview Road<br />
+                  Ottawa, ON  K1Y 2C5<br />
+                  <a href="mailto:team@csedottawa.ca">team@csedottawa.ca</a><br />
+                  <a href="https://twitter.com/csedottawa">@csedottawa</a>
+                </p>
+              </div>
 
-            <div className="col-md-4">
-              <h2>Terms</h2>
+              <div className="col-md-4">
+                <h2>Terms</h2>
 
-              <Link to="/privacy">Privacy policy</Link>
+                <Link to="/privacy">Privacy policy</Link>
+              </div>
             </div>
           </div>
         </div>
